9efa40e84e09ae4d579d87b8a57191095ebb133b
Yann Boucher <Stellaris62@protonmail.com>
2017-08-23 16:27:46 +0200

    Added scripts

---
+++ tools/iso.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+
+cd ..
+ 
+mkdir -p isodir
+mkdir -p isodir/boot
+mkdir -p isodir/boot/grub
+ 
+cp build/bin/LudOS.bin isodir/boot/LudOS.kernel
+cat > isodir/boot/grub/grub.cfg << EOF
+menuentry "LudOS" {
+	multiboot2 /boot/LudOS.kernel
+}
+EOF
+grub-mkrescue -o isodir/LudOS.iso isodir &> /dev/null
---
+++ tools/run.sh
@@ -0,0 +1,3 @@
+set -e
+cd ..
+qemu-system-i386 -cdrom isodir/LudOS.iso 

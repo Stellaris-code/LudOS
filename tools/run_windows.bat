b87cde5b687d6b2dd403720f22e5926ebb46a4e8
Stellaris-code <Stellaris-code@users.noreply.github.com>
2017-11-05 13:18:21 +0100

    Update run_windows.bat

--- tools/run_windows.bat
+++ tools/run_windows.bat
@@ -1,3 +1,3 @@
 cd ..
-"C:\Program Files\qemu\qemu-system-i386.exe" -d mmu,cpu_reset,guest_errors -soundhw pcspk -serial stdio -no-reboot -m 16M -kernel build/bin/LudOS.bin -append "loglevel=info" -drive id=disk,file=build/bin/LudOS.img,if=none -device ahci,id=ahci -device ide-drive,drive=disk,bus=ahci.0 -initrd 'isodir/boot/initrd.img initrd'
+"C:\Program Files\qemu\qemu-system-i386.exe" -d mmu,cpu_reset,guest_errors -soundhw pcspk -serial stdio -no-reboot -m 16M -kernel build/bin/LudOS.bin -append "loglevel=info" -drive id=disk,file=build/bin/LudOS.img,if=none -device ahci,id=ahci -device ide-drive,drive=disk,bus=ahci.0 -initrd 'build/bin/initrd.img initrd'
 cd tools
